2d5493e2ebaba572edf76fbfdc9c817c1757331e
Emily <emilyazepeda@gmail.com>
2019-08-20 13:36:00 -0700

    Data formatting with Eric

--- format data.R
+++ format data.R
@@ -1,32 +1,90 @@
-library(dplyr)
 
-data <- read.csv("data.csv")
+#data is a water striders file that comes from striders something
+#hopefully emily remembers what it is :)
 
-#change mating column to 0 and 1
-data$mating <- ifelse(data$mating == "N" | data$mating == "NA" |data$mating == "ES" |data$mating =="O" |data$mating =="OVI" |data$mating =="V" |data$mating =="DEAD" |data$mating =="C" |data$mating =="dead?" |data$mating == "?", 0, 1)
+df = data
 
-#change activity column to 0 and 1
-data$activity <- ifelse(data$behavior == "W" | data$behavior == "WF" | data$behavior == "WC"| data$behavior == "WES", 1, 0)
+df = df %>% group_by(date, tank, time) %>% 
+  mutate(uniq_ids = paste0(sort(unique(id)), collapse = "_")) %>% ungroup()
 
-#subset data to include only relevant columns
-data <- subset(data, select = c(date, tank, id, sex, time, activity, mating, Treatment))
+tanksdays_changingstriders = df %>% group_by(date, tank) %>% summarize(n = length(unique(uniq_ids))) %>% ungroup()
+#the above dataframe contains the tank ids that contain a 
+#different set of striders within the same day
+#moving forward, we decided to eliminate these observations
+#there are only four-days with changing group membership
+
+tanksdays_changingstriders %>% filter(n > 1) #filter code filters the dataframe to those
+#observations where n is greater than 1
+#       date tank      n
+# <int> <fct> <int>
+# 1 20130502 18        2
+# 2 20130505 D         2
+# 3 20130507 T5        2
+# 4 20130507 T9        2
 
 
+#so, to eliminate these observations, use anti-join
+#removes observations from df which are present in tanksdays...
+df_mod = tanksdays_changingstriders %>% filter(n > 1) %>%
+  select(-n) %>% #this line removes the n column, which we no longer need
+  anti_join(df, ., by = c("date", "tank"))
+
+(nrow(df) - nrow(df_mod))/nrow(df)
+#we lose 0.86% of the data
+
+#df_mod contains observations where group membership does not change on a single day
+#so now, let's calculate the unique groups labels, which is the unique combination
+#of uniq_id and tank id
+df_mod$groupID = paste0("group", df_mod$tank, " ", df_mod$uniq_ids)
 
 
-###Group activity and mating data by id so we have proportion of time mating and active for each individual in each trial
-test <- data %>% group_by(date) %>% group_by(tank)
-  
-  
-newDat <- with(data, split(data, list(date, tank, Treatment), drop = TRUE))
-newDat$
+#now add a trial label to each of the different groups, where trial is just the day
+#that that group is being assayed. We don't technically need this, since we already
+#have the day information, but it's probably useful since we want to think about
+#trial 1 across different groups, even if that trial 1 occurred on different days for
+#those groups
+#to do this, I will group by our new "groupID" variable, and then sort the data
+#within that grouping variable by "date", 'time', and 'id' so that when we add 
+#a new trial column according to date, we can be sure that the earliest date corresponds
+#to the earliest trial
+#this probably isn't necessary, assuming the data are already correctly sorted, but on the
+#off chance that for some data the values are not entered chronologically, this code
+#will prevent that from being an issue
+#note that in the arrange code, the .by_group argument ensures that the data are arranged
+#within their grouping category (rather than as a whole, regardless of grouping)
+df_mod = df_mod %>% group_by(groupID) %>% arrange(date, time, id, .by_group = T) %>%
+  mutate(trial = as.integer(as.factor(date))) %>% ungroup()
+#the as.factor and as.integer code ensures that for different groups with different
+#first dates, that first date gets a trial value of 1 for both groups
 
-ids <- lapply(1:length(newDate), function(z){
-  
-}
-  
-  
-  
-  
-ids <- lapply(newDat, `[`, c('date', 'tank', 'sex', 'id', 'Treatment'))
-id2 <- rbind_list(ids)
+#we have found that sex and treatment are problematic because there are multiple
+#entries of sex and treatment for some groupID-trial-id combinations
+test = df_mod %>% group_by(groupID, trial, id) %>%
+  summarize( n_treatment = length(unique(Treatment)), 
+             n_sex = length(unique(sex))) %>% ungroup()
+
+save(df_mod, file = "./df_mod.rdata")
+load(file = "./df_mod.rdata")
+
+#fix issues with tanks who were assigned large treatment even though they are small 
+df_mod$Treatment <- ifelse(df_mod$groupID == "groupT20 144_157-G_160-R_161-B_39_97", "small", df_mod$Treatment)
+
+#fix sex for individual 145-Y who was assigned male for one entry
+df_mod$sex <- ifelse(df_mod$id == "145-Y", "F", df_mod$sex)
+
+
+#and so now we want to calculate the proportion of time active and mating, within a day,
+#for each male, for each of its observed trials, for each of its observed groups
+#but remember that trial is the same thing as day
+df_mod = df_mod %>% group_by(groupID, trial, id) %>% 
+  summarize(prop_active = sum(activity, na.rm = T)/sum(!is.na(activity)),
+            prop_mating = sum(mating, na.rm = T)/sum(!is.na(mating)),
+            sex = unique(sex),
+            date = unique(date),
+            Treatment = unique(Treatment),
+            tank = unique(tank)) %>% ungroup()
+
+
+#now, to filter to males
+df_mod = df_mod %>% filter(sex == "m")
+
